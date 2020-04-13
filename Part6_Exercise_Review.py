f67193eb81fee21a02a0845bb584e2c918b8140d
anujprj143 <dharmrp.143@gmail.com>
2020-04-13 11:22:01 +0530

    Exercise completed

--- Part6_Exercise_Review.py
+++ Part6_Exercise_Review.py
@@ -72,3 +72,4 @@ name = "Sammy"
 
 # Use print formatting to print the following string:
 "Hello my dog's name is Sammy and he is 4 years old"
+print("Hello my dog's name is {} and he is {} years old".format(name,age))
