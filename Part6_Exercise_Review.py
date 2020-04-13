e87d62931d14ce73d2748c0fb9f6fe80c39f2056
anujprj143 <dharmrp.143@gmail.com>
2020-04-13 11:12:19 +0530

    dictionary based problem solved

--- Part6_Exercise_Review.py
+++ Part6_Exercise_Review.py
@@ -49,8 +49,9 @@ print(d1['simple_key'])
 d2 = {'k1':{'k2':'hello'}}
 print(d2['k1']['k2'])
 
-d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
 
+d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+print(d3['k1'][0]['nest_key'][1][0])
 
 ###############
 ## Problem 4 ##
