048101efbe771b4a638124834c8e74110c7c4031
anujprj143 <dharmrp.143@gmail.com>
2020-06-22 12:01:47 +0530

    Scope of variables in python

--- L2_Part1_Scope.py
+++ L2_Part1_Scope.py
@@ -78,3 +78,101 @@ def greet():
 greet()
 
 
+
+
+# Global
+print name
+
+
+# Built-in
+# These are the built-in function names in Python (can't overwrite these!)
+
+len
+
+
+# Local Variables
+
+# When you declare variables inside a function definition, they are not related
+# in any way to other variables with the same names used outside the function -
+# i.e. variable names are local to the function. This is called the scope of the
+# variable. All variables have the scope of the block they are declared in
+# starting from the point of definition of the name.
+#
+# Example:
+
+x = 50
+
+def func(x):
+    print('x is', x)
+    x = 2
+    print('Changed local x to', x)
+
+func(x)
+print('x is still', x)
+
+
+# The first time that we print the value of the name x with the first line in
+# the function’s body, Python uses the value of the parameter declared in the
+# main block, above the function definition.
+#
+# Next, we assign the value 2 to x. The name x is local to our function. So,
+# when we change the value of x in the function, the x defined in the main block
+# remains unaffected.
+#
+# With the last print statement, we display the value of x as defined in the main
+# block, thereby confirming that it is actually unaffected by the local
+# assignment within the previously called function.
+
+################################
+# The Global Statement
+################################
+
+# If you want to assign a value to a name defined at the top level of the program
+# (i.e. not inside any kind of scope such as functions or classes), then you have
+# to tell Python that the name is not local, but it is global. We do this using
+# the global statement. It is impossible to assign a value to a variable defined
+# outside a function without the global statement.
+#
+# You can use the values of such variables defined outside the function
+# (assuming there is no variable with the same name within the function).
+# However, this is not encouraged and should be avoided since it becomes unclear
+# to the reader of the program as to where that variable’s definition is. Using
+# the global statement makes it amply clear that the variable is defined
+# in an outermost block.
+#
+# Example:
+
+x = 50
+
+def func():
+    global x
+    print('This function is now using the global x!')
+    print('Because of global x is: ', x)
+    x = 2
+    print('Ran func(), changed global x to', x)
+
+print('Before calling func(), x is: ', x)
+func()
+print('Value of x (outside of func()) is: ', x)
+
+
+# The global statement is used to declare that x is a global variable - hence,
+# when we assign a value to x inside the function, that change is reflected
+# when we use the value of x in the main block.
+#
+# You can specify more than one global variable using the same glo0bal statement
+# e.g. global x, y, z.
+
+###############################
+# Conclusion
+###############################
+
+# You should now have a good understanding of Scope (you may have already
+# intuitively felt right about Scope which is great!) One last mention is that
+# you can use the globals() and locals() functions to check what are your current
+# local and global variables.
+#
+# Another thing to keep in mind is that everything in Python is an object! I can
+# assign variables to functions just like I can with numbers! We will go over
+# this again in the decorator section of the course!
+
