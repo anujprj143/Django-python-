96b31d70b8dfceb76ba94b6d9880d4a5fdce4925
anujprj143 <dharmrp.143@gmail.com>
2020-09-02 18:45:21 +0530

    scope of a variable or function in python

--- L2_Part1_Scope.py
+++ L2_Part1_Scope.py
@@ -111,33 +111,19 @@ func(x)
 print('x is still', x)
 
 
-# The first time that we print the value of the name x with the first line in
-# the function’s body, Python uses the value of the parameter declared in the
-# main block, above the function definition.
-#
-# Next, we assign the value 2 to x. The name x is local to our function. So,
-# when we change the value of x in the function, the x defined in the main block
-# remains unaffected.
-#
-# With the last print statement, we display the value of x as defined in the main
-# block, thereby confirming that it is actually unaffected by the local
-# assignment within the previously called function.
-
 ################################
 # The Global Statement
 ################################
 
-# If you want to assign a value to a name defined at the top level of the program
-# (i.e. not inside any kind of scope such as functions or classes), then you have
+# If we want to assign a value to a name defined at the top level of the program
+# (i.e. not inside any kind of scope such as functions or classes), then we have
 # to tell Python that the name is not local, but it is global. We do this using
 # the global statement. It is impossible to assign a value to a variable defined
 # outside a function without the global statement.
 #
-# You can use the values of such variables defined outside the function
+# We can use the values of such variables defined outside the function
 # (assuming there is no variable with the same name within the function).
-# However, this is not encouraged and should be avoided since it becomes unclear
-# to the reader of the program as to where that variable’s definition is. Using
-# the global statement makes it amply clear that the variable is defined
+ #Using the global statement makes it amply clear that the variable is defined
 # in an outermost block.
 #
 # Example:
@@ -160,19 +146,15 @@ print('Value of x (outside of func()) is: ', x)
 # when we assign a value to x inside the function, that change is reflected
 # when we use the value of x in the main block.
 #
-# You can specify more than one global variable using the same glo0bal statement
+# We can specify more than one global variable using the same global statement
 # e.g. global x, y, z.
 
 ###############################
 # Conclusion
 ###############################
 
-# You should now have a good understanding of Scope (you may have already
-# intuitively felt right about Scope which is great!) One last mention is that
-# you can use the globals() and locals() functions to check what are your current
+# One last mention is that
+#we can use the globals() and locals() functions to check what are our current
 # local and global variables.
-#
-# Another thing to keep in mind is that everything in Python is an object! I can
-# assign variables to functions just like I can with numbers! We will go over
-# this again in the decorator section of the course!
+
 
